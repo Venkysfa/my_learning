02da6b00a03a9fcaa8f98bcb7230ce94a2721546
venkyblu <venkatesh.rathod@sfaplay.com>
2023-01-10 17:37:15 +0530

    initial commit

---
+++ python_project/tictac.py
@@ -0,0 +1,36 @@
+import pygame as pg, sys
+from pygame.locals import *
+import time
+
+#initialize global variables
+XO = 'x'
+winner = None
+draw = False
+width = 400
+height = 400
+white = (255,255,255)
+line_color = (10,10,10)
+
+#TicTacToe 3x3 board
+TTT = [[None]*3,[None]*3,[None]*3]
+
+#initializing pygame window
+
+pg.init()
+fps = 30
+CLOCK = pg.time.Clock()
+screen = pg.display.set_mode((width, height+100),0,32)
+pg.display.set_caption("Tic Tac Toe")
+
+#load the images
+opening = pg.image.load('tic tac opening.png')
+x_img = pg.image.load('x.png')
+o_img = pg.image.load('o.png')
+
+#resizing images
+x_img = pg.transform.scale(x_img, (80,80))
+o_img = pg.transform.scale(o_img, (80, 80))
+opening = pg.transform.scale(opening, (width, height+100))
+
+def game_opening():
+    screen.blit()
